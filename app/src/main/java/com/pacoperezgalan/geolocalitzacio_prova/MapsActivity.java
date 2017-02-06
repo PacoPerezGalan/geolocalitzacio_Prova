9ad14361e8ca95ad55fa6e1e3027b06bebcde01a
WINMAC12\2dam <quico.97@gmail.com>
2017-02-06 20:27:47 +0100

    Initial commit

---
+++ app/src/main/java/com/pacoperezgalan/geolocalitzacio_prova/MapsActivity.java
@@ -0,0 +1,63 @@
+package com.pacoperezgalan.geolocalitzacio_prova;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Marker marcador;
+    Double lat=0.0;
+    double lng=0.0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+    }
+
+    public void agregarMarcador(Double lat,Double lng){
+        LatLng coordenades=new LatLng(lat,lng);
+        CameraUpdate mUbicacio=CameraUpdateFactory.newLatLngZoom(coordenades,15);
+        if(marcador==null) marcador.remove();
+        marcador=mMap.addMarker(new MarkerOptions()
+        .position(coordenades)
+        .title("La meua ubicacio")
+        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.animateCamera(mUbicacio);
+    }
+
+
+}
