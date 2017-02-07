239dd08aa0cfd85e18fa1ac4ef14d0e2bdabe853
pacoperezgalan <quico.97@gmail.com>
2017-02-07 14:19:04 +0100

    geo

--- app/src/main/java/com/pacoperezgalan/geolocalitzacio_prova/MapsActivity.java
+++ app/src/main/java/com/pacoperezgalan/geolocalitzacio_prova/MapsActivity.java
@@ -1,7 +1,19 @@
 package com.pacoperezgalan.geolocalitzacio_prova;
 
+
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,12 +25,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.google.android.gms.analytics.internal.zzy.m;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     Marker marcador;
-    Double lat=0.0;
-    double lng=0.0;
+    Double lat = 0.0;
+    double lng = 0.0;
 
 
     @Override
@@ -44,20 +58,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
+        meuaUbicacio();
     }
 
-    public void agregarMarcador(Double lat,Double lng){
-        LatLng coordenades=new LatLng(lat,lng);
-        CameraUpdate mUbicacio=CameraUpdateFactory.newLatLngZoom(coordenades,15);
-        if(marcador==null) marcador.remove();
-        marcador=mMap.addMarker(new MarkerOptions()
-        .position(coordenades)
-        .title("La meua ubicacio")
-        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+    private void agregarMarcador(Double lat, Double lng) {
+        LatLng coordenades = new LatLng(lat, lng);
+        CameraUpdate mUbicacio = CameraUpdateFactory.newLatLngZoom(coordenades, 15);
+        if (marcador != null) marcador.remove();
+
+        marcador = mMap.addMarker(new MarkerOptions()
+                .position(coordenades)
+                .title("La meua ubicacio")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
         mMap.animateCamera(mUbicacio);
     }
 
+    private void actualitzarUbicacio(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            Toast.makeText(this, "Localizacion chachi: " + lat + "   " + lng, Toast.LENGTH_SHORT).show();
+            agregarMarcador(lat, lng);
+        }else{
+            Toast.makeText(this, "location null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualitzarUbicacio(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void meuaUbicacio() {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},2);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "No va la ubicacio ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,15000,0,locationListener);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        actualitzarUbicacio(location);
+
+
+    }
 
 }
