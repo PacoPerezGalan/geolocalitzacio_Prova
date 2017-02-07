a1fdaf3579b8c6d7f0ef8a134fe52c9798a2bb60
pacoperezgalan <quico.97@gmail.com>
2017-02-07 14:19:55 +0100

    geo

--- app/src/main/java/com/pacoperezgalan/geolocalitzacio_prova/MapsActivity.java
+++ app/src/main/java/com/pacoperezgalan/geolocalitzacio_prova/MapsActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 
+
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
